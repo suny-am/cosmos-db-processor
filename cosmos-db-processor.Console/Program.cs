576975b32ed80a56ecfa5537436d1fc79f646271
suny-am <visualarea.1@gmail.com>
2024-04-12 22:08:23 +0000

    fix naming of CosmosDBHandler; populate Program.cs in Console project with starter

--- cosmos-db-processor.Console/Program.cs
+++ cosmos-db-processor.Console/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CosmosDBProcessor.Library;
+
+CosmosDbHandler handler = new();
+
+string? databaseID = Environment.GetEnvironmentVariable("DATABASE_ID");
+string? containerID = Environment.GetEnvironmentVariable("CONTAINER_ID");
+string? partitionKeyPath = Environment.GetEnvironmentVariable("PARTITION_KEY_PATH");
+
+if (databaseID is null || containerID is null || partitionKeyPath is null)
+{
+    throw new Exception("could not load required environment variables");
+}
+
+try
+{
+    await handler.LoadDataSource(databaseID, containerID, partitionKeyPath);
+}
+catch (Exception ex)
+{
+
+    throw new Exception(ex.Message);
+}
+
+Console.WriteLine(handler.Container);
+Console.WriteLine(handler.Database);
+Console.WriteLine(handler.Client);
