088a06c380d1eb69828f0acba98353188febf586
suny-am <visualarea.1@gmail.com>
2024-04-15 17:27:53 +0000

    Add timed tests in Console; slim down AllItems()

--- CosmosDBProcessor.Console/Program.cs
+++ CosmosDBProcessor.Console/Program.cs
@@ -1,6 +1,12 @@
-﻿using CosmosDBProcessor.Library;
+﻿using System.Diagnostics;
+using CosmosDBProcessor.Library;
+var stopWatch = new Stopwatch();
 
+Console.WriteLine("beginning timed tests...");
+stopWatch.Start();
 CosmosDbHandler handler = new();
+stopWatch.Stop();
+Console.WriteLine("Handler creation time in Milliseconds: " + stopWatch.ElapsedMilliseconds); ;
 
 string? databaseID = Environment.GetEnvironmentVariable("DATABASE_ID");
 string? containerID = Environment.GetEnvironmentVariable("CONTAINER_ID");
@@ -11,6 +17,7 @@ if (databaseID is null || containerID is null || partitionKeyPath is null)
     throw new Exception("could not load required environment variables");
 }
 
+stopWatch.Restart();
 try
 {
     await handler.LoadDataSource(databaseID, containerID, partitionKeyPath);
@@ -20,12 +27,16 @@ catch (Exception ex)
 
     throw new Exception(ex.Message);
 }
+stopWatch.Stop();
+Console.WriteLine("Data source load time in Milliseconds: " + stopWatch.ElapsedMilliseconds); ;
 
+stopWatch.Restart();
 var items = await handler.AllItems();
 
 if (items is null)
 {
     Console.WriteLine("no items found");
 }
-
 Console.WriteLine("Item count from response: " + items!.Count);
+stopWatch.Stop();
+Console.WriteLine("Item read time in Milliseconds: " + stopWatch.ElapsedMilliseconds);
